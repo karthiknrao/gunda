e07b8d9077290cf42637732cb71eed67b2aa349b
KarthikRao <karthik@indix.com>
2014-11-27 12:17:06 +0530

    Added Price Extractor

--- AmazonParser.py
+++ AmazonParser.py
@@ -8,11 +8,17 @@ class Amazon():
         self.soup = BeautifulSoup( data, 'lxml' )
 
     def parse( self ):
-        prods = self.soup.findAll( 'a', 'a-link-normal s-access-detail-page a-text-normal' )
-        urls = [ prod['href'] for prod in prods ]
-        return urls
+        prods = self.soup.findAll( 'div', 'a-fixed-left-grid-col a-col-right' )
+        data = []
+        for prod in prods:
+            url = prod.findAll( 'a', 'a-link-normal s-access-detail-page a-text-normal' )[0]['href']
+            price = prod.findAll( 'span', 'a-size-base a-color-price s-price a-text-bold' )[0].text
+            data.append( ( url, price ) )
+        return data
 
 if __name__ == '__main__':
     data = open( sys.argv[1] ).read()
     amz = Amazon( data )
-    print amz.parse()
+    for url, price in amz.parse():
+        print url, price
+    
