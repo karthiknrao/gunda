ab6a0966fc711754c47bcfd9309abf44234dc7a7
KarthikRao <karthik@indix.com>
2014-11-28 10:52:47 +0530

    changes

--- brandMPN.py
+++ brandMPN.py
@@ -32,6 +32,7 @@ def stage2Reduce(key, values):
         valuesList.append(value)
     yield key, len(valuesList)
 
+#########
 def stage3Map( key, value ):
     words = value.split( '\t' )
     if len(words) > 6:
@@ -46,6 +47,27 @@ def stage3Reduce(key, values):
     for value in valuesList:
         yield key, value
 
+########
+def stage4Map(key, value):
+    words = value.split( '\t' )
+    if len(words) > 6:
+        if words[5].find( 'INVALID_UPC' ) == -1:
+            yield words[5], value
+
+def stage4Reduce(key, values):
+    otherstores = []
+    mscdirect = []
+    for value in values:
+        if value.find('www.mscdirect.com' ) != -1:
+            mscdirect.append( value )
+        else:
+            otherstores.append( value )
+    if len( mscdirect ) > 0:
+        mscprod = mscdirect[0]
+        for store in otherstores:
+            yield key, (mscprod, store )
+
+######
 def mapper(key, value):
     if os.environ['stage'] == 'stage1':
         for key, value in stage1Map(key, value):
@@ -55,6 +77,9 @@ def mapper(key, value):
             yield key, value
     elif os.environ['stage'] == 'stage3':
         for key, value in stage3Map(key, value):
+            yield key, value
+    elif os.environ['stage'] == 'stage4':
+        for key, value in stage4Map(key, value):
             yield key, value
 
 def reducer(key, values):
@@ -67,13 +92,17 @@ def reducer(key, values):
     elif os.environ['stage'] == 'stage3':
         for key, value in stage3Reduce(key, values):
             yield key, value
+    elif os.environ['stage'] == 'stage4':
+        for key, value in stage4Reduce(key, values):
+            yield key, value
 
 if __name__ == '__main__':
     if sys.argv[1] == "launch":
 #        hadoopy.launch('/data/MSCDirect', '/data/MSCDirectBrandMPN', '/home/hadoop/MSCDirect/brandMPN/brandMPN.py', remove_output = True, jobconfs={'stage':'stage1'})
 #        hadoopy.launch('/data/MSCDirectBrandMPN', '/data/StoreCounts', '/home/hadoop/MSCDirect/brandMPN/brandMPN.py', remove_output = True, jobconfs={'stage':'stage2'})
-        hadoopy.launch('/data/MSCDirect', '/data/MSCUrlUpc', '/home/hadoop/MSCDirect/brandMPN/brandMPN.py', remove_output = True, jobconfs={'stage':'stage3'})
-        data = hadoopy.readtb('/data/MSCUrlUpc')
+        #hadoopy.launch('/data/MSCDirect', '/data/MSCUrlUpc', '/home/hadoop/MSCDirect/brandMPN/brandMPN.py', remove_output = True, jobconfs={'stage':'stage3'})
+        hadoopy.launch('/data/MSCDirect', '/data/MSCUpcMatches', '/home/hadoop/MSCDirect/brandMPN/brandMPN.py', remove_output = True, jobconfs={'stage':'stage4'})
+        data = hadoopy.readtb('/data/MSCUpcMatches')
         for i, j in data:
             print i, j
     else:
